b29253e3096abbe3b45c0213e6fa5050cb15782e
Rizky <rizky.business7@gmail.com>
2025-09-06 21:25:55 +0700

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     return (
         <>
-            <Header />
+            {/* <Header /> */}
             <NavbarMenu />
             <MainContent />
             <Footer />
